d6fdd0656e1b2b75148922cb26b9509860a7d42d
Eric Brooke <ericbrooke@mac.com>
2016-01-12 14:11:42 -0600

    Time to split into files

--- dino_catalog/dino_store.rb
+++ dino_catalog/dino_store.rb
@@ -5,52 +5,66 @@
 # african_dinosaur_export
 # genus, period, carnivore, weight, walking
 
-# load file
-# parse csv
-# see database contents
+# load file x
+# parse csv x
+# see database contents x
 # search by walking, diet, periods, small or big, or combine
-# look at each dinosaur
-# show reults from search/filter
+# look at each dinosaur x
+# show reults from search/filter x
 # search by parameters
 # export json
-
-# table_print
 
 require 'csv'
 require 'table_print'
 
-# def convert_csv_array_of_hashes(file_location)
-#   csv = CSV.read(file_location, headers: true, header_converters: :symbol, :converters => [:all])
-#   arrayed = csv.to_a
-#   array_and_hashed = arrayed.map{|row| row.to_h}
-# end
+class Dinodex
+  attr_accessor :columns
 
-def csv_to_array(file_location)
-  csv = CSV.parse(File.open(file_location, 'r') { |f| f.read })
-  titles = csv.shift # get the titles
-  titles = titles.map { |f| f.downcase.to_sym}
-  csv.collect { |record| Hash[*titles.zip(record).flatten(1)] } # add title to each field
+  def initialize(filename)
+    @store = csv_to_array(filename)
+    @columns
+  end
+
+  def csv_to_array(file_location)
+    csv = CSV.parse(File.open(file_location, 'r') { |f| f.read })
+    titles = csv.shift
+    titles = titles.map { |f| f.downcase.to_sym }
+    @columns = titles
+    csv.collect { |record| Hash[*titles.zip(record).flatten(1)] }
+  end
+
+  def find(keyword)
+    result = @store.find { |x| x.fetch(:name) == keyword }
+    print_one(result)
+  end
+
+  def select(category, keyword)
+    results = @store.find_all { |x| x.fetch(category.to_sym) == keyword }
+    tp results
+  end
+
+  def print_all
+    tp @store
+  end
+
+  def sort
+    @store.sort
+  end
+
+  def print_one(result)
+    puts "-------------------------"
+    tp [result], name: { width: 100 }
+    tp [result], period: { width: 100 }
+    tp [result], continent: { width: 100 }
+    tp [result], diet: { width: 100 }
+    tp [result], weight_in_lbs: { width: 100 }
+    tp [result], walking: { width: 100 }
+    tp [result], description: { width: 100 }
+  end
 end
 
-def find(store, keyword)
-  store.find { |x| x.fetch(:name) == keyword }
-end
-
-def select(store, category, keyword)
-  store.find_all { |x| x.fetch(category.to_sym) == keyword }
-end
-
-def print_all(store)
-  tp store
-end
-
-def sort(store, category, keyword)
-  # %w{apple pear fig}.sort_by { |word| word.length}
-  store.sort_by { |category.to_sym| }
-end
-
-store = csv_to_array('dinodex.csv')
-print_all(store)
-result = find(store, 'Giganotosaurus')
-results = select(store, 'diet', 'Carnivore')
-print_all(results)
+d = Dinodex.new('dinodex.csv')
+d.print_all
+p d.columns
+p d.find('Giganotosaurus')
+d.select('diet', 'Carnivore')
